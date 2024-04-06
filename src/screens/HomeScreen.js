33f64cdce486ba9c502c2da73c60d7fcd0ef271c
jsc1013 <jsc1013@alu.ubu.es>
2024-04-06 20:38:59 +0200

    feat(HomeScreen): Implemented header layout and logout action

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState, useLayoutEffect } from "react";
-import { Text, StyleSheet, View, TouchableOpacity, Image } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Image,
+  BackHandler,
+  Alert,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useTranslation } from "react-i18next";
 import { Divider } from "react-native-paper";
-
+import { auth, firestoreDB } from "../config/firebase-config";
 import { myColors } from "../constants/Colors";
 
 export default function HomeScreen({ route, navigation }) {
@@ -13,6 +21,43 @@ export default function HomeScreen({ route, navigation }) {
   useEffect(function navigationOptions() {
     navigation.setOptions({ headerShown: false });
   }, []);
+
+  // Sets the back options
+  useEffect(function navigationOptions() {
+    navigation.setOptions({ headerShown: false });
+  }, []);
+
+  // Manage logout
+  const logoutAction = () => {
+    const handleLogout = () => {
+      auth
+        .signOut()
+        .then(() => {
+          navigation.navigate("LoginScreen");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    Alert.alert(
+      t("components.home.logoutAppHeader"),
+      t("components.home.logoutAppMessage"),
+      [
+        {
+          text: t("general.cancel"),
+          onPress: () => null,
+          style: "cancel",
+        },
+        {
+          text: t("general.yes"),
+          onPress: () => {
+            handleLogout();
+          },
+        },
+      ]
+    );
+    return true;
+  };
 
   return (
     <View style={styles.container}>
@@ -29,7 +74,7 @@ export default function HomeScreen({ route, navigation }) {
           <Text style={styles.headerHomeManagementText}>Home</Text>
         </View>
         <View>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={logoutAction}>
             <Image
               source={require("../assets/logout.png")}
               style={styles.headerLogoutImage}
